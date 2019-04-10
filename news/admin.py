bd9336572435d1135493d61779f7da44875ac4b6
imalexianne <imanirax@yahoo.fr>
2019-04-10 06:22:54 +0200

    add admin site registers

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile, Business,Neighborhood, Police , Health, Post
+
+
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Police)
+admin.site.register(Health)
+admin.site.register(Post)
+
 
 # Register your models here.
